75f50b340d442677a66fa5c5a2c422d32d05a675
Joy Ittycheriah <bestyoucanbe@yahoo.com>
2019-07-19 14:27:06 -0500

    Working copy! URL routing from chapter

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,53 +1,18 @@
-// Practice Exercise - Displaying Animals
-// Now that you are passing state from the Kennel to the EmployeeList and LocationList, you're going to list animals now.
+import React, { Component } from "react"
+import NavBar from "./nav/NavBar"
+import ApplicationViews from "./ApplicationViews"
 
-// Create a new array in state in the Kennel component named animals. It will look just like the locations and employees arrays in state. Make sure each animal has an id property.
-// Create an AnimalList component for displaying animals.
-// Update Kennel to pass its animals state to AnimalList and use the appropriate key on this.props to display all animal names.
-
-import React, { Component } from 'react'
-import EmployeeList from "./employee/EmployeeList"
-import LocationList from "./location/LocationList"
-import AnimalList from "./animal/AnimalList"
 import "./Kennel.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+
 
 export default class Kennel extends Component {
-
-employeesFromAPI = [
-    { id: 1, name: "Jessica Younker" },
-    { id: 2, name: "Jordan Nelson" },
-    { id: 3, name: "Zoe LeBlanc" },
-    { id: 4, name: "Blaise Roberts" }
-]
-
-locationsFromAPI = [
-    { id: 1, name: "Nashville North", address: "500 Circle Way" },
-    { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-]
-
-animalsFromAPI = [
-    { id: 1, name: "Fido" },
-    { id: 2, name: "Spot" },
-    { id: 3, name: "Cleo" },
-    { id: 4, name: "Lucy" },
-    { id: 5, name: "Bruno"},
-    { id: 6, name: "Sara"}
-]
-
-state = {
-    employees: this.employeesFromAPI,
-    locations: this.locationsFromAPI,
-    animals: this.animalsFromAPI
-}
-
     render() {
         return (
-            <div>
-                <h3>Student Kennels</h3>
-                <LocationList locations={this.state.locations} />
-                <EmployeeList employees={this.state.employees} />
-                <AnimalList animals={this.state.animals} />
-            </div>
+            <React.Fragment>
+                <NavBar />
+                <ApplicationViews />
+            </React.Fragment>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from "react-dom"
 import React from 'react'
+import { BrowserRouter as Router } from "react-router-dom"
 import Kennel from "./components/Kennel"
 
-ReactDOM.render(<Kennel />, document.querySelector("#root"));
+ReactDOM.render(
+  <Router>
+      <Kennel />
+</Router>,
+  document.querySelector("#root"))
+
 
